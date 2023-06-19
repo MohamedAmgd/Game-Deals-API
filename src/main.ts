44279963cd3b0961ce1555f393a87b9b79d625bb
Mohamed Amgd <megapof2@gmail.com>
2023-06-20 01:51:24 +0300

    minor logging

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 3000
   await app.listen(PORT);
+  console.log(`Listening on PORT ${PORT}`);
+
 }
 bootstrap();
