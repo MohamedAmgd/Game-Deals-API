0e4dd539615efdaf7650585fa630fe83b6c05375
Mohamed Amgd <megapof2@gmail.com>
2025-11-15 18:36:32 +0200

    Added deals' images proxying endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,27 @@
 import { CacheInterceptor } from '@nestjs/cache-manager';
-import { Controller, Get, Query, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Query, Res, UseInterceptors } from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   @UseInterceptors(CacheInterceptor)
   getDeals(
-    @Query("free") free?: boolean,
-    @Query("page") page?: number,
-    @Query("title") title?: string
+    @Query('free') free?: boolean,
+    @Query('page') page?: number,
+    @Query('title') title?: string,
   ) {
     return this.appService.getDeals(free, page, title);
+  }
+
+  // Endpoint to proxy deals' images
+  @Get('image')
+  async getImage(@Query('url') url: string, @Res() res: Response) {
+    const image = await this.appService.getImage(url);
+    res.setHeader('Content-Type', image.contentType); // or the appropriate image content type
+    res.send(image.buffer);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,74 +1,124 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable, NotFoundException } from '@nestjs/common';
 import * as cheerio from 'cheerio';
-import { firstValueFrom } from 'rxjs';
+import { buffer, firstValueFrom } from 'rxjs';
+import * as https from 'https';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly httpService: HttpService) { }
+  constructor(private readonly httpService: HttpService) {}
 
   async getDeals(free?: boolean, page?: number, title?: string) {
-    let deals_url = this.getDealsUrl(free, page, title)
+    let deals_url = this.getDealsUrl(free, page, title);
 
+    const result = await firstValueFrom(this.httpService.get(deals_url)).catch(
+      (err) => {
+        console.log(`Error fetching deals from ${deals_url} reason: ${err}`);
+        console.log(
+          `Cookie Header:${this.httpService.axiosRef.defaults.headers.Cookie}`,
+        );
+        console.log(
+          `Cookie Environment Variable: ${process.env.GG_DEALS_COOKIE}`,
+        );
 
-    const result = await firstValueFrom(this.httpService.get(deals_url)).catch((err) => {
-
-      console.log(`Error fetching deals from ${deals_url} reason: ${err}`);
-      console.log(`Cookie Header:${this.httpService.axiosRef.defaults.headers.Cookie}`);
-      console.log(`Cookie Environment Variable: ${process.env.GG_DEALS_COOKIE}`);
-      
-      throw new NotFoundException()
-    })
-    const $ = cheerio.load(result.data)
-    const deals_list_container = $('div[id="deals-list"]')
-    const deals_list = $('.list-items', deals_list_container).children()
-    const deals = []
-    const no_results = deals_list_container
-      .find('div[class="d-flex flex-column wrap_items list emptyProvider"]').text() != ""
+        throw new NotFoundException();
+      },
+    );
+    const $ = cheerio.load(result.data);
+    const deals_list_container = $('div[id="deals-list"]');
+    const deals_list = $('.list-items', deals_list_container).children();
+    const deals = [];
+    const no_results =
+      deals_list_container
+        .find('div[class="d-flex flex-column wrap_items list emptyProvider"]')
+        .text() != '';
     if (no_results) {
       return deals;
     }
     deals_list.each((_index, element) => {
+      const name = $('div[class="game-info-title-wrapper"]', element).text();
+      const old_price = $(
+        'span[class="price-label price-old"]',
+        element,
+      ).text();
+      const new_price = $(
+        'span[class="price-inner game-price-new"]',
+        element,
+      ).text();
+      const discount_percentage = $(
+        'span[class="discount label"]',
+        element,
+      ).text();
+      const expiry_date = $('span[class="expiry label"]', element)
+        .find('time')
+        .text();
 
-      const name = $('div[class="game-info-title-wrapper"]', element).text()
-      const old_price = $('span[class="price-label price-old"]', element).text()
-      const new_price = $('span[class="price-inner game-price-new"]', element).text()
-      const discount_percentage = $('span[class="discount label"]', element).text()
-      const expiry_date = $('span[class="expiry label"]', element).find("time").text()
-
-      let image_url = $('a[class="main-image"]', element).find('img').attr("src")
-      if (image_url?.includes("data:")) {
-        image_url = $('a[class="main-image"]', element).find('img').attr("data-src")
+      let image_url = $('a[class="main-image"]', element)
+        .find('img')
+        .attr('src');
+      if (image_url?.includes('data:')) {
+        image_url = $('a[class="main-image"]', element)
+          .find('img')
+          .attr('data-src');
       }
+      const original_image_url = image_url?.replace('154x72', '308x144');
+      const new_image_url = new URL(
+        process.env.APP_URL || 'http://localhost:3000',
+      );
+      new_image_url.pathname = '/image';
+      new_image_url.searchParams.set('url', original_image_url || '');
+      image_url = new_image_url.toString();
 
-      const discount_url = $('a[class="d-flex flex-align-center flex-justify-center label-with-arrows action-btn-full-box action-btn cta-label-desktop with-arrows action-ext"]', element)
-        .attr('href')
+      const discount_url = $(
+        'a[class="d-flex flex-align-center flex-justify-center label-with-arrows action-btn-full-box action-btn cta-label-desktop with-arrows action-ext"]',
+        element,
+      ).attr('href');
 
-      const is_historical_low: boolean = $('span[class="historical label"]', element).text() == "Historical low"
+      const is_historical_low: boolean =
+        $('span[class="historical label"]', element).text() == 'Historical low';
       deals.push({
         name: name,
         old_price: old_price,
         new_price: new_price,
         discount_percentage: discount_percentage,
         expiry_date: expiry_date,
-        image_url: image_url?.replace("154x72", "308x144"),
-        discount_url: "https://gg.deals".concat(discount_url),
-        is_historical_low: is_historical_low
-      })
-    })
-    return deals
+        image_url: image_url,
+        original_image_url: original_image_url,
+        discount_url: 'https://gg.deals'.concat(discount_url),
+        is_historical_low: is_historical_low,
+      });
+    });
+    return deals;
   }
   getDealsUrl(free?: boolean, page?: number, title?: string): string {
-    let deals_url = "https://gg.deals/deals/?minRating=1"
+    let deals_url = 'https://gg.deals/deals/?minRating=1';
     if (free) {
-      deals_url = deals_url.concat("&maxPrice=0")
+      deals_url = deals_url.concat('&maxPrice=0');
     }
     if (page) {
-      deals_url = deals_url.concat(`&page=${page}`)
+      deals_url = deals_url.concat(`&page=${page}`);
     }
     if (title) {
-      deals_url = deals_url.concat(`&title=${title}`)
+      deals_url = deals_url.concat(`&title=${title}`);
     }
-    return deals_url
+    return deals_url;
+  }
+
+  async getImage(url: string) {
+    const imageRequestAgent = new https.Agent({
+      host: '167.99.223.123',
+      port: 443,
+      servername: 'img.gg.deals',
+    });
+    const result = await firstValueFrom(
+      this.httpService.get(url, {
+        responseType: 'arraybuffer',
+        httpsAgent: imageRequestAgent,
+      }),
+    ).catch((err) => {
+      console.log(`Error fetching image from ${url} reason: ${err}`);
+      throw new NotFoundException();
+    });
+    return { buffer: result.data, contentType: result.headers['content-type'] };
   }
 }
